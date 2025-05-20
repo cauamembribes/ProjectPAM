7e6d8e6e8d4e77dc043063e6efb446564949dcf3
Cauã Membribes <168303859+cauamembribes@users.noreply.github.com>
2025-05-20 22:45:26 +0000

    att

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,32 +1,118 @@
-import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function Index() {
-  const nome = "joao vitor";
-  return <View>
-    <Text>Jogo da Velha</Text>
-    <Text>Vencedor: </Text>
+  const nome = "Cauã";
 
-    <View>
-      <View><TouchableOpacity><Text>X</Text></TouchableOpacity></View>
-      <View><TouchableOpacity><Text>X</Text></TouchableOpacity></View>
-      <View><TouchableOpacity><Text>X</Text></TouchableOpacity></View>
-      <View><TouchableOpacity><Text>X</Text></TouchableOpacity></View>
-      <View><TouchableOpacity><Text>X</Text></TouchableOpacity></View>
-      <View><TouchableOpacity><Text>X</Text></TouchableOpacity></View>
-      <View><TouchableOpacity><Text>X</Text></TouchableOpacity></View>
-      <View><TouchableOpacity><Text>X</Text></TouchableOpacity></View>
-      <View><TouchableOpacity><Text>X</Text></TouchableOpacity></View>
+  return <View style={styles.container}>
+
+    {/* <View>
+      <Text>Jogo da Velha</Text>
+      <Text>Vencedor: </Text>
+    </View> */}
+    
+    <View style={styles.gameBox}>
+        <View style={styles.game}>
+
+            <View>
+              <TouchableOpacity>
+                <Text style={styles.text}>X</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View>
+              <TouchableOpacity>
+                <Text style={styles.text}>X</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View>
+              <TouchableOpacity>
+                <Text style={styles.text}>X</Text>
+              </TouchableOpacity>
+            </View>
+
+        </View>
+
+        <View style={styles.game}>
+
+            <View>
+              <TouchableOpacity>
+                <Text style={styles.text}>X</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View>
+              <TouchableOpacity>
+                <Text style={styles.text}>X</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View>
+              <TouchableOpacity>
+                <Text style={styles.text}>X</Text>
+              </TouchableOpacity>
+            </View>
+
+        </View>
+
+        <View style={styles.game}>
+
+            <View>
+              <TouchableOpacity>
+                <Text style={styles.text}>X</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View>
+              <TouchableOpacity>
+                <Text style={styles.text}>X</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View>
+              <TouchableOpacity>
+                <Text style={styles.text}>X</Text>
+              </TouchableOpacity>
+            </View>
+            
+        </View>
+
     </View>
-
-
+  
   </View>
 
 } 
 
 
 const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
 
-  
+      },
 
+      gameBox: {
+        height: 400,
+        width: 400,
+        padding: 20
+      },
+
+      game: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        backgroundColor: 'grey'
+      },
+
+      text: {
+        fontSize: 60,
+        fontFamily: "monospace"
+
+      },
+
+      
+      
 
 })
